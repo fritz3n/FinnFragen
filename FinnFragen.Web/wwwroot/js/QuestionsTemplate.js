dc06bf1e2d43fae36950397cf7cd841b7c41bcf7
fritzen <finn-johannes.blumenstock@student.kit.edu>
2022-11-16 14:47:26 +0100

    Changed Questions View

--- FinnFragen.Web/wwwroot/js/QuestionsTemplate.js
+++ FinnFragen.Web/wwwroot/js/QuestionsTemplate.js
@@ -12,9 +12,15 @@ function htmlForQuestion(q) {
     return `        
         <li class="list-group-item d-flex flex-row flex-wrap">
             <h5 class="px-3 flex-grow-1 mt-3"><a href="/Questions/Question/${encodeURI(q.shortName)}">${escapeHtml(q.title)}</a></h5>
+            <p class="mx-3 mt-3">${new Date(q.answerDate).toLocaleDateString("de-DE")}</p>
             <p class="small col-12 px-3">${escapeHtml(q.synopsis)}</p>
             <div class="px-3 flex-grow-1">
                 ${tags}
+            <div class="d-flex flex-row flex-grow-1">
+                <div class="px-3 flex-shrink-1">
+                    ${tags}
+                </div>
+                <p class="text-muted px-3 text-end flex-grow-1 text-nowrap">von ${escapeHtml(q.name)}</p>
             </div>
             <p class="text-muted px-3 text-right">von ${escapeHtml(q.name)}</p>
         </li>`;
